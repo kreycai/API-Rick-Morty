f6699f387b2a69a6f5235af8230127674c952453
Guilherme Reale <guireale@hotmail.com>
2022-07-14 17:04:22 -0300

    swagger

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { connectDatabase } from './database/database.js';
 import { routerUsers } from './users/users.route.js';
 import { routerAuth } from './auth/auth.route.js';
 import { routerChar } from './Characters/characters.route.js';
-//
+import { router } from './swagger/swagger.routes.js';
+
 const port = process.env.PORT || 3001;
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use('/users', routerUsers);
 app.use('/auth', routerAuth);
 app.use('/characters', routerChar);
+app.use('/api-docs', router);
 
 app.listen(port, () => {
   console.log(`porta ${port}`);
